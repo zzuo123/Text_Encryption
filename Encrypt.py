2579a9448a800ed756cb351f6d147cbe438e1ff8
Zhiyang Zuo (George) <33331699+zzuo123@users.noreply.github.com>
2021-01-27 14:51:07 -0500

    Updated file path
    Bug fix

--- Encrypt.py
+++ Encrypt.py
@@ -1,26 +1,9 @@
-"""
-This is a text encryption program made by Zhiyang Zuo
-How to use:
-1. create a .txt file
-2. copy and past .txt file location to code
-3. copy and past text to .txt file
-4. run the method encrypt (A two digit password will be copied to your machine and printed out, memorize it!)
-5. encrypted text will be written to your original file
-6. to decrypt, run decrypt method with the two digit password
-7. decrypted text will be written to your original file
--------------------------------------------------------------------------------------------
-----------THIS IS NOT THE SAFEST WAY TO ENCRYPT YOUR TEXT, USE AT YOUR OWN RISK !----------
------------------------------------------ENJOY!--------------------------------------------
--------------------------------------------------------------------------------------------
-"""
-
-
 import pyperclip
 import random
 import sys
 
 
-file_path = "C:\George\PyCharm_Files\Text_Encryption\\text.txt"
+file_path = ".\text.txt"
 
 
 def read():
